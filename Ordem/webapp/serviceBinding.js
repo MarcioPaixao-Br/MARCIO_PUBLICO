2d54a37c11a93a960427befe374a4cde43419a6b
MarcioPaixao-Br <83726513+MarcioPaixao-Br@users.noreply.github.com>
2023-10-20 10:33:54 -0300

    Delete Ordem directory

--- Ordem/webapp/serviceBinding.js
+++
@@ -1,5 +0,0 @@
-function initModel() {
-	var sUrl = "/kendo-ui/";
-	var oModel = new sap.ui.model.odata.ODataModel(sUrl, true);
-	sap.ui.getCore().setModel(oModel);
-}
--- Ordem/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"br/com/ordem/Ordem/test/unit/controller/App.controller"
-], function () {
-	"use strict";
-});
